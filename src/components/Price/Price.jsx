fa7c877ac770831220f3da69dba75d8e319408dc
Alexandrbig1 <alexsmagin1@gmail.com>
2024-04-05 18:41:00 -0500

    fix: updated price components styles

--- src/components/Price/Price.jsx
+++ src/components/Price/Price.jsx
@@ -1,20 +1,52 @@
 function Price() {
   return (
     <div>
-      <div className="card">
-        <h2>Title 1</h2>
-        <p>Price 1</p>
-        <p>Description 1</p>
-      </div>
-      <div className="card">
-        <h2>Title 2</h2>
-        <p>Price 2</p>
-        <p>Description 2</p>
-      </div>
-      <div className="card">
-        <h2>Title 3</h2>
-        <p>Price 3</p>
-        <p>Description 3</p>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <div
+          style={{
+            border: "1px solid black",
+            padding: "10px",
+            margin: "10px",
+            width: "300px",
+          }}
+        >
+          <h3>Title 1</h3>
+          <p>Price: $10</p>
+          <p>
+            Description: Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit.
+          </p>
+        </div>
+        <div
+          style={{
+            border: "1px solid black",
+            padding: "10px",
+            margin: "10px",
+            width: "300px",
+          }}
+        >
+          <h3>Title 2</h3>
+          <p>Price: $20</p>
+          <p>
+            Description: Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit.
+          </p>
+        </div>
+        <div
+          style={{
+            border: "1px solid black",
+            padding: "10px",
+            margin: "10px",
+            width: "300px",
+          }}
+        >
+          <h3>Title 3</h3>
+          <p>Price: $30</p>
+          <p>
+            Description: Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit.
+          </p>
+        </div>
       </div>
     </div>
   );
