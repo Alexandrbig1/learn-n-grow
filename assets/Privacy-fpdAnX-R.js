56270fd8af47dff31cb011be03983e3a123677f1
Alexandrbig1 <alexsmagin1@gmail.com>
2024-05-15 13:33:47 -0500

    Updates

--- assets/Privacy-fpdAnX-R.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CS7j66gg.js";function t(){return r.jsx("div",{children:"Privacy"})}export{t as default};
--- assets/Terms-B-fXUwN-.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CS7j66gg.js";function s(){return r.jsx("div",{children:"Terms"})}export{s as default};
